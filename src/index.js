5abe4253c6a76f78f908ce58fe046498dc98200c
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-04-23 13:01:02 +0300

    Added chenges, part 2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
-import {store} from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 
 import App from './App.jsx';
 
@@ -13,9 +14,11 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,12 +1,23 @@
 import { combineReducers } from 'redux';
+
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import counter from './counter/reducer';
 import counterToolkit from './counter-toolkit/reducer';
 import counterToolkitSlice from './counterToolkitSlice';
 import todos from './todos/reducer';
 
+const persistConfig = {
+  key: 'todos',
+  storage,
+};
+
+const todosReducer = persistReducer(persistConfig, todos);
+
 export default combineReducers({
   counter,
   counterToolkit,
   counterToolkitSlice,
-  todos,
+  todos: todosReducer,
 });
